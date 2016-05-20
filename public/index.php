e27b0ae313857783dac943d2971421f31b822d57
Robbert Klarenbeek <robbertkl@renbeek.nl>
2016-05-20 12:51:58 +0200

    Tiny HTML fixes

--- public/index.php
+++ public/index.php
@@ -141,23 +141,17 @@ $nextInvoiceDate = date('d-m-Y');
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Invoices</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-        <script>
-            $('#tabs a').click(function (e) {
-                e.preventDefault()
-                $(this).tab('show')
-            })
-        </script>
         <style>
             .glyphicon {
                 width: 14px;
                 height: 14px;
             }
         </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </head>
     <body>
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="/">Invoice generator</a>
